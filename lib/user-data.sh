860917fde657facfa707dc76cdd5617c02d878ef
entest <hai@entest.io>
2022-09-04 04:54:44 +0000

    update user-data

--- lib/user-data.sh
+++ lib/user-data.sh
@@ -2,7 +2,7 @@
 cd ~
 wget https://github.com/entest-hai/vpc-sg-ec2-demo/archive/refs/heads/main.zip
 unzip main.zip
-cd vpc-sg-ec2-demo
+cd vpc-sg-ec2-demo-main
 cd web
-python3 -m pip install -r requirements.txt
+python3 -m pip install --user -r requirements.txt
 python3 -m app
