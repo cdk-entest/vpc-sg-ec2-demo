0858bbde7e68431aa0d4e0bcfa4747594b9b86fc
entest <hai@entest.io>
2022-11-19 23:15:04 +0000

    update app.py

--- web/app.py
+++ web/app.py
@@ -1,3 +1,9 @@
+"""
+haimtran 19 NOV 2022
+add host and username
+run port 80 needs amind permisson
+"""
+
 import os
 import socket
 from flask import Flask, render_template
@@ -8,19 +14,26 @@ app = Flask(
 
 
 try:
-    USER_NAME = os.environ['USER_NAME']
+    USER_NAME = os.environ["USER_NAME"]
 except:
     USER_NAME = "Hai Tran"
 
-# @app.route("/")
-# def hello_world():
-#     return app.send_static_file("index.html")
-
 
 @app.route("/")
+def hello_world():
+    """
+    home page
+    """
+    return app.send_static_file("index.html")
+
+
+@app.route("/host")
 def get_host():
+    """
+    host page
+    """
     host = socket.gethostname()
-    return render_template("host.html", host=host, username = USER_NAME)
+    return render_template("host.html", host=host, username=USER_NAME)
 
 
 if __name__ == "__main__":
