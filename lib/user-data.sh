f4cedca1e9246a470618b1414f3bdf5e97075fe2
entest <hai@entest.io>
2022-11-19 08:26:08 +0000

    update userdata

--- lib/user-data.sh
+++ lib/user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~
-wget https://github.com/entest-hai/vpc-sg-ec2-demo/archive/refs/heads/main.zip
+https://github.com/cdk-entest/vpc-sg-ec2-demo/archive/refs/heads/main.zip
 unzip main.zip
 cd vpc-sg-ec2-demo-main
 cd web
